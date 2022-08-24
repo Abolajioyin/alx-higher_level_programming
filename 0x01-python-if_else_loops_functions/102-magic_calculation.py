ab42531c9e4196b1ffcf75c1090b717127557798
Abolajioyin <bolajioyinloye@yahoo.com>
2022-08-24 04:54:10 -0700

    another commit

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-def remove_char_at(str, n):
-    str_copy = list()
-    for i in range(len(str)):
-        if i == n:
-            continue
-        else:
-            str_copy.append(str[i])
-    return "".join(str_copy)
+def magic_calculation(a, b, c):
+    if a < b:
+        return c
+    elif c > b:
+        return a + b
+    return a * b - c
