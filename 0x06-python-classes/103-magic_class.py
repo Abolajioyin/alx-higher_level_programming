1974de307c00e429399161e94741d911902effb5
Abolajioyin <bolajioyinloye@yahoo.com>
2022-09-14 00:52:30 -0700

    another commit

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
+
+"""Define a class MagicClass"""
+
 import math
 
 
 class MagicClass:
-    ''' Magic Class reverses given Class to bytecode'''
-
+    """MagicClass class"""
     def __init__(self, radius=0):
+        """Initialize a new class"""
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
-            raise TypeError('radius must be a number')
+            raise TypeError("radius must be a number")
         self.__radius = radius
 
     def area(self):
-        return self.__radius**2 * math.pi
+        """Area"""
+        return self.__radius ** 2 * math.pi
 
     def circumference(self):
+        """Circumference"""
         return 2 * math.pi * self.__radius
